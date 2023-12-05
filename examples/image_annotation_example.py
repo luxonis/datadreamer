df0ad08c81b2961898bab0748bfc737b01b458c6
Nikita Sokovnin <nikitoos67@gmail.com>
2023-12-05 03:01:17 +0000

    feature: update examples

--- examples/image_annotation_example.py
+++ examples/image_annotation_example.py
@@ -1,34 +1,59 @@
 from PIL import Image
-from basereduce.dataset_annotation import OWLv2Annotator, ModelName, TaskList
+from basereduce.dataset_annotation import OWLv2Annotator, TaskList, Kosmos2Annotator
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import numpy as np
 
 # Initialize the OWLv2Annotator
 annotator = OWLv2Annotator(
     seed=42,
-    model_name=ModelName.OWL_V2,
-    task_definition=TaskList.OBJECT_DETECTION,
     device="cuda",  # Use "cuda" for GPU or "cpu" for CPU
 )
 
 # Load your image
-image = Image.open("../images/dalle-logo.png").convert("RGB")
+image = Image.open("generated_images/Figure_1.png").convert("RGB")
+
 
 
 # Define prompts to guide the detection
-prompts = ["Eye", "Text"]
+#prompts = ["Eye", "Text"]
+
+class_map = {
+    "Austronaut": "astronaut",
+    "Spaceman": "astronaut",
+    "Cosmonaut": "astronaut",
+    "Moonwalker": "astronaut",
+    "Horse": "horse",
+    "Stablemate": "horse",
+    "Steed": "horse",
+    "Moon": "moon",
+}
+
+# class_map = {
+#     "Austronaut": "astronaut",
+#     "Horse": "horse",
+#     "Moon": "moon",
+# }
+
+prompts = list(class_map.keys())
 
 # Perform object detection
-boxes, scores, labels = annotator.annotate(image, prompts, conf_threshold=0.1)
+boxes, scores, labels = annotator.annotate(image, prompts, conf_threshold=0.2, use_tta=True)
 
 # Convert to numpy arrays
-boxes = boxes.detach().cpu().numpy()
-scores = scores.detach().cpu().numpy()
-labels = labels.detach().cpu().numpy()
+if not isinstance(boxes, np.ndarray):
+    boxes = boxes.detach().cpu().numpy()
+if not isinstance(scores, np.ndarray):
+    scores = scores.detach().cpu().numpy()
+if not isinstance(labels, np.ndarray):
+    labels = labels.detach().cpu().numpy()
 
 # Process the results
 for box, score, label in zip(boxes, scores, labels):
-    print(f"Box: {box}, Score: {score}, Label: {prompts[label]}")
+    if isinstance(label, np.int64):
+        print(f"Box: {box}, Score: {score}, Label: {class_map[prompts[label]]}")
+    else:
+        print(f"Box: {box}, Score: {score}, Label: {label}")
 
 
 fig, ax = plt.subplots(1)
@@ -44,10 +69,13 @@ for box, score, label in zip(boxes, scores, labels):
     )
     ax.add_patch(rect)
 
+    if isinstance(label, np.int64):
+        label = class_map[prompts[label]]
+
     plt.text(
         x1,
         y1,
-        f"{prompts[label]} {score:.2f}",
+        f"{label} {score:.2f}",
         bbox=dict(facecolor="yellow", alpha=0.5),
     )
 
--- examples/prompt_generation_example.py
+++ examples/prompt_generation_example.py
@@ -1,10 +1,10 @@
-from basereduce.prompt_generation.prompt_generator import LMPromptGenerator, LMName
+from basereduce.prompt_generation import LMPromptGenerator
 
 
 if __name__ == "__main__":
     object_names = ["aeroplane", "bicycle", "bird", "boat"]
     prompt_generator = LMPromptGenerator(
-        class_names=object_names, model_name=LMName.MISTRAL
+        class_names=object_names
     )
     generated_prompts = prompt_generator.generate_prompts()
     for prompt in generated_prompts:
