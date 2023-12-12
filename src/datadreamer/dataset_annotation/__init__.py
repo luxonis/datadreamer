d4acf7a1d46667e23b57b84ee78157cfa875933f
Nikita Sokovnin <49622375+sokovninn@users.noreply.github.com>
2023-12-12 12:51:46 +0100

    fix: remove kosmos2 import

--- src/datadreamer/dataset_annotation/__init__.py
+++ src/datadreamer/dataset_annotation/__init__.py
@@ -1,5 +1,4 @@
 from .image_annotator import BaseAnnotator, TaskList
-from .kosmos2_annotator import Kosmos2Annotator
 from .owlv2_annotator import OWLv2Annotator
 
-__all__ = ["BaseAnnotator", "TaskList", "OWLv2Annotator", "Kosmos2Annotator"]
+__all__ = ["BaseAnnotator", "TaskList", "OWLv2Annotator"]
