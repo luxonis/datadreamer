5913a91133adf1fa19b3e0ed010ff2c5ef2a7b28
Jan Cuhel <jan.cuhel@protonmail.com>
2023-12-27 02:14:04 +0100

    Add test structure

---
+++ tests/unittests/test_image_generation.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_clip_image_tester():
+    pass
+
+
+def test_sdxl_image_generator():
+    pass
+
+
+def test_sdxl_turbo_image_generator():
+    pass
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ tests/unittests/test_prompt_generation.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_simple_prompt_generator():
+    pass
+
+
+def test_lm_prompt_generator():
+    pass
+
+
+def test_synonym_generator():
+    pass
+
+
+if __name__ == "__main__":
+    pytest.main()
