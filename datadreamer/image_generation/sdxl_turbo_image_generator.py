f41d311cda9af92bb4663dee767fb67dc5591e06
klemen1999 <klemenskrlj8@gmail.com>
2026-06-16 13:35:52 +0200

    Fix compel package, pin versions

--- datadreamer/image_generation/sdxl_turbo_image_generator.py
+++ datadreamer/image_generation/sdxl_turbo_image_generator.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from typing import List, Optional
 
 import torch
-from compel import Compel, ReturnedEmbeddingsType
+from compel.compel import Compel
+from compel.embeddings_provider import ReturnedEmbeddingsType
 from diffusers import AutoPipelineForText2Image
 from loguru import logger
 from PIL import Image
